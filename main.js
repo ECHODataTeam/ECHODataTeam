2aad0c2fc0e8709a7e940184b242068d134d8a79
Scott Gunn <sgunn@salud.unm.edu>
2023-04-17 11:23:56 -0600

    waterfall chart

--- main.js
+++ main.js
@@ -1,49 +1,107 @@
-Chart.defaults.global.defaultFontFamily = 'Roboto';
-Chart.defaults.global.defaultFontColor = '#333';
+Chart.defaults.defaultFontFamily = 'Roboto';
+Chart.defaults.defaultFontColor = '#333';
 
-function makeChart(players) {
-  // players is an array of objects where each object is something like:
-  // {
-  //   "Name": "Steffi Graf",
-  //   "Weeks": "377",
-  //   "Gender": "Female"
-  // }
+//load waterfall_chart data
+d3.csv("waterfall_chart.csv").then((data) => {
+  const parsedData = data.map((d) => ({
+    year: d.year,
+    value: +d.total,
+  }));
 
-  var playerLabels = players.map(function(d) {return d.Name});
-  var weeksData = players.map(function(d) {return +d.Weeks});
-  var playerColors = players.map(function(d) {return d.Gender === 'Female' ? '#F15F36' : '#19A0AA';});
+  createWaterfallChart(parsedData);
+});
 
-  var chart = new Chart('chart', {
-    type: 'horizontalBar',
-    options: {
-      maintainAspectRatio: false,
-      legend: {
-        display: false
-      },
-      scales: {
-        xAxes: [
-          {
-            scaleLabel: {
-              display: true,
-              labelString: 'Weeks at No.1',
-              fontSize: 16
-            }
-          }
-        ]
-      }
-    },
+function createWaterfallChart(parsedData) {
+  Chart.register(ChartDataLabels);
+
+  const labels = parsedData.map((d) => d.year);
+  labels.push("Total Attendances");
+
+  const data = parsedData.map((d) => d.value);
+
+  const runningTotalData = data.reduce((acc, curr, index) => {
+    acc.push((index > 0 ? acc[index - 1] : 0) + curr);
+    return acc;
+  }, []);
+
+  const grandTotal = runningTotalData[runningTotalData.length - 1];
+
+  const stackedData = data.map((value, index) => {
+    return index > 0 ? [runningTotalData[index - 1], value] : [0, value];
+  });
+
+  stackedData.push([0, grandTotal]);
+
+  const ctx = document.getElementById('waterfall-chart').getContext('2d');
+  const myChart = new Chart(ctx, {
+    type: 'bar',
     data: {
-      labels: playerLabels,
+      labels: labels,
       datasets: [
         {
-          data: weeksData,
-          backgroundColor: playerColors
-        }
-      ]
-    }
-  })
-}
-
-// Request data using D3
-d3.csv('https://s3-us-west-2.amazonaws.com/s.cdpn.io/2814973/atp_wta.csv')
-  .then(makeChart);
+          label: 'Previous Total',
+          data: stackedData.map(value => value[0]),
+          backgroundColor: 'rgba(0, 0, 0, 0)',
+          borderColor: 'rgba(0, 0, 0, 0)',
+        },
+        {
+          label: 'Attendances',
+          data: stackedData.map(value => value[1]),
+          backgroundColor: (context) => {
+            const index = context.dataIndex;
+            if (index === data.length) {
+              return '#007A86'; // Grand total
+            }
+            return '#007A86';
+          },
+          borderColor: (context) => {
+            const index = context.dataIndex;
+            if (index === data.length) {
+              return 'black'; // Grand Total
+            }
+            return 'black';
+          },
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+          stacked: true,
+          grid: {
+            drawOnChartArea: false 
+          }
+        },
+        x: {
+          stacked: true,
+          grid: {
+            drawOnChartArea: false 
+          }
+        },
+      },
+      plugins: {
+        datalabels: {
+          color: 'black',
+          formatter: (value, context) => {
+            const index = context.dataIndex;
+            const datasetIndex = context.datasetIndex;
+            if (datasetIndex === 0) {
+              return ''; 
+            }
+            if (index === data.length) {
+              return grandTotal; 
+            }
+            return value; 
+          },
+          align: 'end',
+          anchor: 'end',
+        },
+        legend: {
+          display: false,
+        },
+      },
+    },
+  });
+};
